006759cfdb980640ae30a693d89576601db4f462
Emily Pancake <epancake24@gmail.com>
2018-01-28 10:22:58 -0700

    heroku deploy

--- app.js
+++ app.js
@@ -7,72 +7,102 @@ const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (request, response) => {
+    queries.list("questions")
+      .then(questions =>
+        queries.list("solvers").then(solvers => queries.list("questions_solvers").then(questions_solvers =>
+        response.json({
+          questions: questions,
+          solvers: solvers,
+          questions_solvers: questions_solvers
+        })
+      ))
+      )
+    .catch(console.error)
+});
+
 app.get("/questions", (request, response) => {
-    queries.listqs().then(questions => {
-        response.json({questions});
-    }).catch(console.error);
+  queries.list("questions")
+  .then(questions => {
+    response.json({ questions });
+  })
+  .catch(console.error);
+});
+
+app.get("/solvers", (request, response) => {
+  queries.list("solvers")
+  .then(solvers => {
+    response.json({ solvers });
+  })
+  .catch(console.error);
+});
+
+app.get("/questions_solvers", (request, response) => {
+  queries.list("questions_solvers")
+  .then(questions_solvers => {
+    response.json({ questions_solvers });
+  })
+  .catch(console.error);
 });
 
 app.get("/questions/:id", (request, response) => {
-    queries.readqs(request.params.id).then(question => {
+    queries.read("questions", request.params.id).then(question => {
         question
             ? response.json({question})
             : response.sendStatus(404)
     }).catch(console.error);
 });
 
+app.get("/solvers/:id", (request, response) => {
+    queries.read("solvers", request.params.id).then(solvers => {
+        solver
+            ? response.json({solver})
+            : response.sendStatus(404)
+    }).catch(console.error);
+});
+
+app.get("/questions_solvers/:id", (request, response) => {
+    queries.read("questions_solvers", request.params.id).then(questions_solvers => {
+        question_solver
+            ? response.json({question_solver})
+            : response.sendStatus(404)
+    }).catch(console.error);
+});
+
+
 app.post("/questions", (request, response) => {
-    queries.createqs(request.body).then(question => {
+    queries.create("questions", request.body).then(question => {
         response.status(201).json({questions});
     }).catch(console.error);
 });
 
+app.post("/solvers", (request, response) => {
+    queries.create("solvers", request.body).then(solvers => {
+        response.status(201).json({solvers});
+    }).catch(console.error);
+});
+
+app.post("/questions_solvers", (request, response) => {
+    queries.create("questions_solvers", request.body).then(questions_solvers => {
+        response.status(201).json({questions_solvers});
+    }).catch(console.error);
+});
+
 app.delete("/questions/:id", (request, response) => {
-    queries.deleteqs(request.params.id).then(() => {
+    queries.delete("questions", request.params.id).then(() => {
         response.sendStatus(204);
     }).catch(console.error);
 });
 
 app.put("/questions/:id", (request, response) => {
-    queries.updateqs(request.params.id, request.body).then(question => {
+    queries.update(request.params.id, request.body).then(question => {
         response.json({question});
     }).catch(console.error);
 });
 
-app.get("/solvers", (request, response) => {
-    queries.listSolvers().then(solvers => {
-        response.json({solvers});
-    }).catch(console.error);
-});
-
-app.get("/solvers/:id", (request, response) => {
-    queries.readSolvers(request.params.id).then(question => {
-        question
-            ? response.json({question})
-            : response.sendStatus(404)
-    }).catch(console.error);
-});
-
-app.post("/solvers", (request, response) => {
-    queries.createSolvers(request.body).then(question => {
-        response.status(201).json({question});
-    }).catch(console.error);
-});
-
-app.delete("/solvers/:id", (request, response) => {
-    queries.deleteSolvers(request.params.id).then(() => {
-        response.sendStatus(204);
-    }).catch(console.error);
-});
-
-app.put("/solvers/:id", (request, response) => {
-    queries.updateSolvers(request.params.id, request.body).then(question => {
-        response.json({solver});
-    }).catch(console.error);
-});
 
 app.use((request, response) => {
-    response.send(404);
+    response.sendStatus(404);
 });
 
 module.exports = app;
--- queries.js
+++ queries.js
@@ -1,42 +1,23 @@
 const database = require("./database-connection");
 
 module.exports = {
-  listqs() {
-    return database("questions").select();
+  list(table) {
+    return database(table).select();
   },
-  readqs(id) {
-    return database('questions').select().where("id", id).first()
+  read(table, id) {
+    return database(table).select().where("id", id).first()
   },
-  createqs(question) {
-    return database('questions')
-      .insert(question)
+  create(table, item) {
+    return database(table)
+      .insert(item)
       .returning("*")
       .then(record => record[0])
   },
-  updateqs(id, question) {
-    return database('questions').update(question)
+  update(table, id, question) {
+    return database(table).update(item)
       .where("id", id).returning("*").then(record => record[0])
   },
-  deleteqs(id) {
-    return database('questions').delete().where("id", id)
-  },
-  listSolvers() {
-    return database("solvers").select();
-  },
-  readSolvers(id) {
-    return database('solvers').select().where("id", id).first()
-  },
-  createSolvers(game) {
-    return database('solvers')
-      .insert(game)
-      .returning("*")
-      .then(record => record[0])
-  },
-  updateSolvers(id, solver) {
-    return database('solvers').update(solver)
-      .where("id", id).returning("*").then(record => record[0])
-  },
-  deleteSolvers(id) {
-    return database('solvers').delete().where("id", id)
+  delete(table, id) {
+    return database(table).delete().where("id", id)
   }
-};
+}
