951051fbc57dbcdd827f18926472ccfa26b2bdfe
Emily Pancake <epancake24@gmail.com>
2018-02-12 13:12:47 -0700

    add morgan

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const queries = require("./queries");
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
 const cors = require("cors");
 
-app.use(cors());
+app.use(morgan('dev'))
+app.use(cors({origin: true}));
 app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
